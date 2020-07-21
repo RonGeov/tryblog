56d9eb516547884af63b5549656e8335c1c17510
Ron George Valiyaveettil <ron2george@gmail.com>
2020-07-21 19:19:52 +0530

    Added category navbar link
    added category drop-down in navigation bar

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, CategoryListView, ArticleView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
 
 urlpatterns = [
     #path('',views.home, name="home"),
@@ -11,7 +11,8 @@ urlpatterns = [
     path('add_category/',AddCategoryView.as_view(),name = "add_category"),
     path('article/edit/<int:pk>',UpdatePostView.as_view(),name="update_post"),
     path('article/<int:pk>/delete',DeletePostView.as_view(),name="delete_post"),
-    path('category/<str:cats>/',CategoryView,name="category")
+    path('category/<str:cats>/',CategoryView,name="category"),
+    path('category_list/',CategoryListView,name="category_list")
 
 
 ]
