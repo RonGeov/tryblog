90a11e61c46a733692ff2a3bbcf5a1ef01c9a120
Ron George Valiyaveettil <ron2george@gmail.com>
2020-07-24 19:30:17 +0530

    stylized edit user profile page

--- members/views.py
+++ members/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm,EditProfileForm
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
-class UserEditView(generic.CreateView):
-    form_class = UserChangeForm
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
